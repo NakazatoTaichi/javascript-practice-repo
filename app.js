6c101f3b54d31116c47174ec81b4fa094d7a57a8
NakazatoTaichi <taichi.nakazato0521@gmail.com>
2025-10-11 04:18:05 +0900

    練習問題

--- app.js
+++ app.js
@@ -1,11 +1,4 @@
-// windowに対するaddEventListenerはWebページ全体に対するイベント
-// window.addEventListener("イベントタイプ", "イベント時に実行する関数")
-window.addEventListener("load", function(){
-    alert("読み込み完了");
-})
-
-// documentに対するaddEventListenerは個々のHTML要素に関するイベントを設定（ボタンや画像など）
-// document.addEventListener()
 document.querySelector("#button").addEventListener("click", function(){
-    console.log("クリックされた！");
+    $output = document.querySelector("#output");
+    $output.textContent = "ピカチュウ";
 });
