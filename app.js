061fd9d7d8ca46ea672c5fec69a10702be7f260e
NakazatoTaichi <taichi.nakazato0521@gmail.com>
2025-10-11 04:21:51 +0900

    練習問題２

--- app.js
+++ app.js
@@ -1,4 +1,8 @@
 document.querySelector("#button").addEventListener("click", function(){
-    $output = document.querySelector("#output");
-    $output.textContent = "ピカチュウ";
+    $rand = Math.random();
+    if($rand <= 0.2) {
+        console.log("ピカチュウをゲットした！");
+    } else {
+        console.log("残念！もう少しで捕まえられたのに！");
+    }
 });
